ec7a1b77bf016ceacce629204414736640ac6d49
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-02-25 00:41:06 -0300

    Realizar operação de deposito

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -1,5 +1,8 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
+  alias Rocketpay.Accounts.Deposit
  #facade pattern
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate deposit(params), to: Deposit, as: :call
+
 end
